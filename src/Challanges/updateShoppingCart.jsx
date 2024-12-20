0b8aa0ed79a7c16ec26cda2c9abf6547daadf1c8
madeha <madehayoussof96@gmail.com>
2024-12-20 23:12:24 +0300

    Update an item in the shopping cart

---
+++ src/Challanges/updateShoppingCart.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 10,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    const newProduct = products.map((item) => {
+      if (item.id === productId) {
+        return { ...item, count: (item.count += 1) };
+      }
+      return item;
+    });
+
+    setProducts(newProduct);
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
