df93dd72113cf5e45f466b305883768f1e116ac4
madeha <madehayoussof96@gmail.com>
2024-12-20 22:48:20 +0300

    edit array state

--- src/Lessons/arrayAsState.jsx
+++ src/Lessons/arrayAsState.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 let id = 3;
+
 const ArrayAsState = () => {
   const [item, setItem] = useState("");
+  const [editedItem, setEditedItem] = useState("");
   const [devices, setDevices] = useState([
     { id: 0, name: "Mac" },
     { id: 1, name: "Windows" },
@@ -10,8 +12,17 @@ const ArrayAsState = () => {
   ]);
 
   const handleDelete = (id) => {
-    console.log("id", id);
     setDevices(devices.filter((device) => device.id !== id));
+  };
+
+  const handleEdit = (id) => {
+    const newDevices = devices.map((device) => {
+      if (device.id === id) {
+        return { ...device, name: device.name + "0" };
+      }
+      return device;
+    });
+    setDevices(newDevices);
   };
 
   const devicesList = devices.map((device) => (
@@ -19,6 +30,7 @@ const ArrayAsState = () => {
       {device.name}
       <div style={{ textAlign: "right" }}>
         <button onClick={() => handleDelete(device.id)}>Delete</button>
+        <button onClick={() => handleEdit(device.id)}>Edit</button>
       </div>
     </li>
   ));
