2e34a1007b1b5114be9748d4a214e21f1fbd7a50
madeha <madehayoussof96@gmail.com>
2024-12-20 21:36:14 +0300

    delete in array state

--- src/Lessons/arrayAsState.jsx
+++ src/Lessons/arrayAsState.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 
+let id = 3;
 const ArrayAsState = () => {
   const [item, setItem] = useState("");
-  const [devices, setDevices] = useState(["Mac", "Windows", "Linux"]);
-  const devicesList = devices.map((device, index) => (
-    <li key={index}>{device}</li>
+  const [devices, setDevices] = useState([
+    { id: 0, name: "Mac" },
+    { id: 1, name: "Windows" },
+    { id: 2, name: "Linux" },
+  ]);
+
+  const handleDelete = (id) => {
+    console.log("id", id);
+    setDevices(devices.filter((device) => device.id !== id));
+  };
+
+  const devicesList = devices.map((device) => (
+    <li key={device.id}>
+      {device.name}
+      <div style={{ textAlign: "right" }}>
+        <button onClick={() => handleDelete(device.id)}>Delete</button>
+      </div>
+    </li>
   ));
 
   return (
@@ -19,7 +35,14 @@ const ArrayAsState = () => {
       {devicesList}
       <hr />
       <input value={item} onChange={(e) => setItem(e.target.value)} />
-      <button onClick={() => setDevices([...devices, item])}>Add</button>
+      <button
+        onClick={() => {
+          setDevices([...devices, { id: id + 1, name: item }]);
+          id++;
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 };
